1cfcb8dbd2246c7949ae292363935ef9299bc6f4
Zetta Eric Julianto <algonacci1@gmail.com>
2022-06-30 11:28:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, request, jsonify
 import json
 from QuestionAnswering import get_answer_using_bert
@@ -18,6 +17,29 @@ def index():
 def post():
     data = request.get_json()
     return jsonify(data)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        input_json = request.get_json()
+        query = input_json['query']
+        context_list = input_json['context_list']
+        for val in context_list:
+            context = val['context']
+            context = context.replace('\n', '')
+            prediction = get_answer_using_bert(context, query)
+            json = {
+                'results': [
+                    {
+                        'answers': prediction,
+                        'id': val['id'],
+                        'question': query,
+                    }
+                ]
+            }
+        return jsonify(json)
+    except Exception as e:
+        return {'error': str(e)}
 
 if __name__ == '__main__':
     app.run()
